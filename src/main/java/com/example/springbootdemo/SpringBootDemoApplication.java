e7ec40dd2d805c8f2020dd49b39799320a49cabc
Cindy1o1 <709334661@qq.com>
2023-12-18 15:00:16 +0800

    第二次提交

--- src/main/java/com/example/springbootdemo/SpringBootDemoApplication.java
+++ src/main/java/com/example/springbootdemo/SpringBootDemoApplication.java
@@ -11,6 +11,7 @@ public class SpringBootDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootDemoApplication.class, args);
         System.out.println("llll");
+        System.out.println("gggg");
     }
 
 }
